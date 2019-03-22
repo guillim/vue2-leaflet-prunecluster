9e3d76ba427f44d8fc37b5604e144902341b9e81
Guillim <guillaume.lancrenon@commoprices.com>
2019-03-22 10:26:34 +0100

    bug fix

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      globalReturn: false,
+    },
+    // babelOptions: {
+    //   configFile: "path/to/config.js",
+    // },
+  },
+};
